9903e542786c2e5cea2b8fc330d63a5b2df188a0
A-4S <dev.forsythe@gmail.com>
2025-03-25 23:01:22 -0400

    Update pipe.py; added docstring

--- src/pipepy/pipe.py
+++ src/pipepy/pipe.py
@@ -1,5 +1,24 @@
 from functools import reduce
 from typing import Callable
 
-def pipe(*f: list[Callable]) -> Callable:
+
+def pipe(*f: Callable) -> Callable:
+    """
+    ## Description
+    Functionally streamlines a series of function calls.
+
+    ## Example
+    .. code-block:: python
+        def title_case(msg: str) -> list[str]:
+            return pipe(
+                str.lower,
+                str.title,
+                str.split
+            )(msg)
+    
+    **Returns**
+    >>> title_case('WHY, HELLO THERE! 😊')
+    >>> ['Why,', 'Hello', 'There!', '😊']
+    """
+
     return lambda x: reduce(lambda a, f: f(a), f, x)

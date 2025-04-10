4d0461c4e8e2e671b1bcb29ec29cb0d1032e95fd
A-4S <dev.forsythe@gmail.com>
2025-04-10 17:07:19 +0000

    Updated pipe.py; now supports type hints

--- src/pipe_fp/pipe.py
+++ src/pipe_fp/pipe.py
@@ -1,8 +1,111 @@
 from functools import reduce
-from typing import Callable
+from typing import Any, Callable, overload
+
+from .type import (
+    T0,
+    T1,
+    T2,
+    T3,
+    T4,
+    T5,
+    T6,
+    T7,
+    T8,
+    T9,
+)
 
 
-def pipe(*f: Callable) -> Callable:
+@overload
+def pipe(
+    f1: Callable[[T0], T1],
+) -> T1: ...
+
+
+@overload
+def pipe(
+    f1: Callable[[T0], T1],
+    f2: Callable[[T1], T2],
+) -> T2: ...
+
+
+@overload
+def pipe(
+    f1: Callable[[T0], T1],
+    f2: Callable[[T1], T2],
+    f3: Callable[[T2], T3],
+) -> T3: ...
+
+
+@overload
+def pipe(
+    f1: Callable[[T0], T1],
+    f2: Callable[[T1], T2],
+    f3: Callable[[T2], T3],
+    f4: Callable[[T3], T4],
+) -> T4: ...
+
+
+@overload
+def pipe(
+    f1: Callable[[T0], T1],
+    f2: Callable[[T1], T2],
+    f3: Callable[[T2], T3],
+    f4: Callable[[T3], T4],
+    f5: Callable[[T4], T5],
+) -> T5: ...
+
+
+@overload
+def pipe(
+    f1: Callable[[T0], T1],
+    f2: Callable[[T1], T2],
+    f3: Callable[[T2], T3],
+    f4: Callable[[T3], T4],
+    f5: Callable[[T4], T5],
+    f6: Callable[[T5], T6],
+) -> T6: ...
+
+
+@overload
+def pipe(
+    f1: Callable[[T0], T1],
+    f2: Callable[[T1], T2],
+    f3: Callable[[T2], T3],
+    f4: Callable[[T3], T4],
+    f5: Callable[[T4], T5],
+    f6: Callable[[T5], T6],
+    f7: Callable[[T6], T7],
+) -> T7: ...
+
+
+@overload
+def pipe(
+    f1: Callable[[T0], T1],
+    f2: Callable[[T1], T2],
+    f3: Callable[[T2], T3],
+    f4: Callable[[T3], T4],
+    f5: Callable[[T4], T5],
+    f6: Callable[[T5], T6],
+    f7: Callable[[T6], T7],
+    f8: Callable[[T7], T8],
+) -> T8: ...
+
+
+@overload
+def pipe(
+    f1: Callable[[T0], T1],
+    f2: Callable[[T1], T2],
+    f3: Callable[[T2], T3],
+    f4: Callable[[T3], T4],
+    f5: Callable[[T4], T5],
+    f6: Callable[[T5], T6],
+    f7: Callable[[T6], T7],
+    f8: Callable[[T7], T8],
+    f9: Callable[[T8], T9],
+) -> T9: ...
+
+
+def pipe(*f: Callable[..., Any]) -> Callable:
     """
     ## Description
     Functionally streamlines a series of function calls.
@@ -15,7 +118,7 @@ def pipe(*f: Callable) -> Callable:
                 str.title,
                 str.split
             )(msg)
-    
+
     **Returns**
     >>> title_case('WHY, HELLO THERE! 😊')
     ['Why,', 'Hello', 'There!', '😊']

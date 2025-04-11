8cf79c425571934664ff66d7d750b0750d5e3bc7
A-4S <dev.forsythe@gmail.com>
2025-04-11 20:15:45 +0000

    Updated pipe.py; 1. pipe function is now a call-only class 2. now fully compatible with lambda type hinting 3. restricted to a max of 9 functions 4. switched from depricated typing.Callable

--- src/pipe_fp/pipe.py
+++ src/pipe_fp/pipe.py
@@ -1,111 +1,11 @@
+from collections.abc import Callable, Iterable
 from functools import reduce
-from typing import Any, Callable, overload
+from typing import Any, Generic, overload
 
-from .type import (
-    T0,
-    T1,
-    T2,
-    T3,
-    T4,
-    T5,
-    T6,
-    T7,
-    T8,
-    T9,
-)
+from .type import T0, T1, T2, T3, T4, T5, T6, T7, T8, T9
 
 
-@overload
-def pipe(
-    f1: Callable[[T0], T1],
-) -> T1: ...
-
-
-@overload
-def pipe(
-    f1: Callable[[T0], T1],
-    f2: Callable[[T1], T2],
-) -> T2: ...
-
-
-@overload
-def pipe(
-    f1: Callable[[T0], T1],
-    f2: Callable[[T1], T2],
-    f3: Callable[[T2], T3],
-) -> T3: ...
-
-
-@overload
-def pipe(
-    f1: Callable[[T0], T1],
-    f2: Callable[[T1], T2],
-    f3: Callable[[T2], T3],
-    f4: Callable[[T3], T4],
-) -> T4: ...
-
-
-@overload
-def pipe(
-    f1: Callable[[T0], T1],
-    f2: Callable[[T1], T2],
-    f3: Callable[[T2], T3],
-    f4: Callable[[T3], T4],
-    f5: Callable[[T4], T5],
-) -> T5: ...
-
-
-@overload
-def pipe(
-    f1: Callable[[T0], T1],
-    f2: Callable[[T1], T2],
-    f3: Callable[[T2], T3],
-    f4: Callable[[T3], T4],
-    f5: Callable[[T4], T5],
-    f6: Callable[[T5], T6],
-) -> T6: ...
-
-
-@overload
-def pipe(
-    f1: Callable[[T0], T1],
-    f2: Callable[[T1], T2],
-    f3: Callable[[T2], T3],
-    f4: Callable[[T3], T4],
-    f5: Callable[[T4], T5],
-    f6: Callable[[T5], T6],
-    f7: Callable[[T6], T7],
-) -> T7: ...
-
-
-@overload
-def pipe(
-    f1: Callable[[T0], T1],
-    f2: Callable[[T1], T2],
-    f3: Callable[[T2], T3],
-    f4: Callable[[T3], T4],
-    f5: Callable[[T4], T5],
-    f6: Callable[[T5], T6],
-    f7: Callable[[T6], T7],
-    f8: Callable[[T7], T8],
-) -> T8: ...
-
-
-@overload
-def pipe(
-    f1: Callable[[T0], T1],
-    f2: Callable[[T1], T2],
-    f3: Callable[[T2], T3],
-    f4: Callable[[T3], T4],
-    f5: Callable[[T4], T5],
-    f6: Callable[[T5], T6],
-    f7: Callable[[T6], T7],
-    f8: Callable[[T7], T8],
-    f9: Callable[[T8], T9],
-) -> T9: ...
-
-
-def pipe(*f: Callable[..., Any]) -> Callable:
+class pipe(Generic[T0]):
     """
     ## Description
     Functionally streamlines a series of function calls.
@@ -113,7 +13,7 @@ def pipe(*f: Callable[..., Any]) -> Callable:
     ## Example
     .. code-block:: python
         def title_case(msg: str) -> list[str]:
-            return pipe(
+            return pipe[str](
                 str.lower,
                 str.title,
                 str.split
@@ -124,4 +24,95 @@ def pipe(*f: Callable[..., Any]) -> Callable:
     ['Why,', 'Hello', 'There!', '😊']
     """
 
-    return lambda x: reduce(lambda a, f: f(a), f, x)
+    @overload
+    def __new__(
+        cls,
+        f1: Callable[[T0], T1],
+    ) -> Callable[[T0], T0]: ...
+
+    @overload
+    def __new__(
+        cls,
+        f1: Callable[[T0], T1],
+        f2: Callable[[T1], T2],
+    ) -> Callable[[T0], T2]: ...
+
+    @overload
+    def __new__(
+        cls,
+        f1: Callable[[T0], T1],
+        f2: Callable[[T1], T2],
+        f3: Callable[[T2], T3],
+    ) -> Callable[[T0], T3]: ...
+
+    @overload
+    def __new__(
+        cls,
+        f1: Callable[[T0], T1],
+        f2: Callable[[T1], T2],
+        f3: Callable[[T2], T3],
+        f4: Callable[[T3], T4],
+    ) -> Callable[[T0], T9]: ...
+
+    @overload
+    def __new__(
+        cls,
+        f1: Callable[[T0], T1],
+        f2: Callable[[T1], T2],
+        f3: Callable[[T2], T3],
+        f4: Callable[[T3], T4],
+        f5: Callable[[T4], T5],
+    ) -> Callable[[T0], T9]: ...
+
+    @overload
+    def __new__(
+        cls,
+        f1: Callable[[T0], T1],
+        f2: Callable[[T1], T2],
+        f3: Callable[[T2], T3],
+        f4: Callable[[T3], T4],
+        f5: Callable[[T4], T5],
+        f6: Callable[[T5], T6],
+    ) -> Callable[[T0], T9]: ...
+
+    @overload
+    def __new__(
+        cls,
+        f1: Callable[[T0], T1],
+        f2: Callable[[T1], T2],
+        f3: Callable[[T2], T3],
+        f4: Callable[[T3], T4],
+        f5: Callable[[T4], T5],
+        f6: Callable[[T5], T6],
+        f7: Callable[[T6], T7],
+    ) -> Callable[[T0], T9]: ...
+
+    @overload
+    def __new__(
+        cls,
+        f1: Callable[[T0], T1],
+        f2: Callable[[T1], T2],
+        f3: Callable[[T2], T3],
+        f4: Callable[[T3], T4],
+        f5: Callable[[T4], T5],
+        f6: Callable[[T5], T6],
+        f7: Callable[[T6], T7],
+        f8: Callable[[T7], T8],
+    ) -> Callable[[T0], T9]: ...
+
+    @overload
+    def __new__(
+        cls,
+        f1: Callable[[T0], T1],
+        f2: Callable[[T1], T2],
+        f3: Callable[[T2], T3],
+        f4: Callable[[T3], T4],
+        f5: Callable[[T4], T5],
+        f6: Callable[[T5], T6],
+        f7: Callable[[T6], T7],
+        f8: Callable[[T7], T8],
+        f9: Callable[[T8], T9],
+    ) -> Callable[[T0], T9]: ...
+
+    def __new__(cls, *f: Callable[..., Any]) -> Callable[..., Any]:
+        return lambda x: reduce(lambda a, c: c(a), f, x)
